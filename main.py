1cefa5b061d5fdf027d8806df3c9a64b558e484a
Joey Shi <joeyshi12@gmail.com>
2021-12-07 18:01:09 -0800

    Minor update to match yt docs boilerplate better

--- main.py
+++ main.py
@@ -19,17 +19,23 @@ scopes = ["https://www.googleapis.com/auth/youtube.force-ssl"]
 # views, likes, dislikes, ratio, date
 STATISTICS_COMMENT = "This is an automated comment to display likes & dislikes for the video you're currently watching, since YouTube decided to disable the dislike count on videos. \nViews: {views}\nLikes: {likes}\nDislikes: {dislikes}\nRatio: {ratio}%\nLast Updated: {date}\nYouTube, please don't ban or shadowban me. I learned how to do this from your own docs. \nLol thanks."
 
-def build_api_client(api_service_name: str, api_version: str, client_secrets_file: str) -> YoutubeClientAdapter:
+def build_youtube_client() -> YoutubeClientAdapter:
     # Disable OAuthlib's HTTPS verification when running locally.
     # *DO NOT* leave this option enabled in production.
     # os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
+
+    api_service_name = "youtube"
+    api_version = "v3"
+    client_secrets_file = "YOUR_CLIENT_SECRET_FILE.json"
+
     flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(client_secrets_file, scopes)
     credentials = flow.run_console()
     client = googleapiclient.discovery.build(api_service_name, api_version, credentials=credentials)
+
     return YoutubeClientAdapter(client, API_KEY)
 
 def main():
-    client: YoutubeClientAdapter = build_api_client("youtube", "v3", "YOUR_CLIENT_SECRET_FILE.json")
+    client: YoutubeClientAdapter = build_youtube_client()
     for video in client.get_channel_videos(CHANNEL_ID):
         ratio = 0 if video.like_count == 0 else round(100 * video.like_count / (video.like_count + video.dislike_count))
         date = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
